1677d76238536bf7d7ceffe135cbd330f23d783e
Dave <haasdp@gmail.com>
2022-02-27 14:49:55 -0500

    works!

---
+++ index.js
@@ -0,0 +1,10 @@
+const PublicGoogleSheetsParser = require('public-google-sheets-parser')
+
+const spreadsheetId = '1aH5dtvYAYwxPML8keeznPeWSAdcT9kgzwlK6bRKWUsI';
+
+// 1. You can pass spreadsheetId when parser instantiation
+const parser = new PublicGoogleSheetsParser(spreadsheetId)
+parser.parse().then((items) => {
+    console.log(items);
+  // items should be [{"a":1,"b":2,"c":3},{"a":4,"b":5,"c":6},{"a":7,"b":8,"c":9}]
+})
