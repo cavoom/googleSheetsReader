409b47371a75642cea277d7edd5cc32612eb7cad
Dave <haasdp@gmail.com>
2022-02-27 22:11:09 -0500

    grabbing items from spreasheet working

--- index.js
+++ index.js
@@ -14,46 +14,84 @@ var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 // Define as an empty object here
 // Then redefine
 var params = {};
-params = {
-  TableName: 'admr_questions',
-  Item: {
-    'id' : {S: 'mikea56el'},
-    'sport' : {S: 'bingoss'},
-    'thing' : {N: '89'}
-  }
-};
+var numRecords = 0;
+var theBigArray = [];
+
+// For Test
+// Moving this to buildIt fn
+// params = {
+//   TableName: 'admr_questions',
+//   Item: {
+//     'id' : {S: 'mikea56el'},
+//     'sport' : {S: 'bingoss'},
+//     'thing' : {N: '89'}
+//   }
+// };
 
 // This is the lambda function
 exports.handler = function(event, context, callback) {
   // 1. You can pass spreadsheetId when parser instantiation
 const parser = new PublicGoogleSheetsParser(spreadsheetId)
 parser.parse().then((items) => {
-  console.log('length of items array:',items.length);
-  console.log('camp name of 0:', items[0].campaignName);
-  //testerArray.push(items);
+  //console.log('length of items array:',items.length);
+  //console.log('camp name of 0:', items[0].campaignName);
+  
+  // Call function to build params here
+  // then nest analytics call underneith
+  buildIt(items,(theBigArray)=>{
+    console.log('here is the array to build params',theBigArray.length);
+    console.log('an item:', theBigArray[0].PutRequest.Item.id);
 
-  analytics(items, (stuff)=>{
-    //console.log('all done analytics with:',stuff);
-    callback(null, items);
-        });
+    callback(null, params);
 
-  // callback(null, items);
-})
+
+  // analytics(items, (stuff)=>{
+  //   //console.log('all done analytics with:',stuff);
+  //   callback(null, items);
+  //       });
+
+})  // end builtIt
+}) // end parser
 
 
 }
 
-// DYNAMO
+// DYNAMO Batchwrite function
 function analytics(items, callback){
-ddb.putItem(params, function(err, data) {
+
+ddb.batchWriteItem(params, function(err, data) {
   if (err) {
     console.log("Error", err);
     callback(err);
   } else {
     console.log("Success", data);
     callback(data)
-  }
-});
-
-
+    }
+  })
 }
+
+function buildIt(items, callback){
+  var i = 0;
+  var tempObject = {};
+  numRecords = items.length;
+  //console.log('number of items: ', numRecords);
+  for(i=0;i<numRecords;i++){
+    tempObject = {
+      PutRequest: {
+        Item: {
+          "id": { "S": items[i].uniqueID},
+            "order": { "N": items[i].order },
+            "campaignName": { "S": items[i].campaignName}
+        }
+      }
+    } // tempObject
+    theBigArray.push(tempObject);
+    } // for loop
+    callback(theBigArray);
+    //callback(params)
+  }
+
+  
+
+
+
